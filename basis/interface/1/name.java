3b1f4e310921d2797fa911af0213bfc37fd7e9ca
1637094807 <1637094807@outlook.com>
2020-05-07 15:37:16 +0800

    继承

--- basis/interface/1/name.java
+++ basis/interface/1/name.java
@@ -6,7 +6,7 @@ package com.company;
 *   public abstract 返回值类型  方法名称(参数列表)
 *
 * 注意事项
-* total.接口当中的抽象方法，修饰符必须是两个固定的关键字: public abstract
+* 1.接口当中的抽象方法，修饰符必须是两个固定的关键字: public abstract
 * 2.这两个关键字修饰符，可以选择性省略
 * 3.方法的三要素，可以随意定义。
 * */
