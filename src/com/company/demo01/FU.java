6143869391773ad64cf54a1579da772c9b453143
1637094807 <1637094807@outlook.com>
2020-05-11 23:27:35 +0800

    1

---
+++ src/com/company/demo01/FU.java
@@ -0,0 +1,10 @@
+package com.company.demo01;
+
+public class FU {
+    public void method(){
+        System.out.println("父类方法");
+    }
+    public void methodfu(){
+        System.out.println("父类特有");
+    }
+}
--- src/com/company/demo01/main.java
+++ src/com/company/demo01/main.java
@@ -1,18 +1,26 @@
 package com.company.demo01;
 
-import org.w3c.dom.ls.LSOutput;
-
-/*
+/*multi
 *
-* 1.类与类之间是单继承的。直接父类只有一个。
-* 2.类与接口之间是多实现的，一个类可以实现多个接口。
-* 3.接口与接口之间是多继承的。
+* 代码当中体现多态性，其实就是一句话：父类引用指向子类对象。
+* 格式:
+* 父类名称 对象名=new 子类名称();
+* 或者
+* 接口名称  对象名= new 实现类名称();
 *
 *
 * */
 public class main {
+    public static void main(String[] args) {
+        //使用多态的写法
+        //左侧父类的引用，指向了右侧子类的对象
+        FU obj=new ZI();
 
-//    11
+        obj.method();
+        obj.methodfu();
+
+
+    }
 
 
 }
