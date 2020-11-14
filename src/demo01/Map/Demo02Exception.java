62936d746fabbde0a0ff8eae26cdfde31660af9b
1637094807 <1637094807@outlook.com>
2020-11-14 20:04:36 +0800

    4

--- src/demo01/Map/Demo02Exception.java
+++ src/demo01/Map/Demo02Exception.java
@@ -27,4 +27,18 @@ public class Demo02Exception {
         return ele;
     }
 
+    /*
+    * 访问了数组中的3索引，而数组是没有3索引的，者时候jvm就会检测出程序会出现异常
+    * JVM会做两件事情
+    *           1.JVM会根据异常产生的原因创建一个异常对象，这个异常对象包含了异常产生的(内容,位置，原因)
+    *                   new ArrayIndexOutOfBoundsException("3");
+    *           2.在getElement方法中，没有异常的处理逻辑(try catch),那么JAVM就会把异常对象抛出给方法的调用者
+    *           Main来处理这个异常
+    *
+    *
+    *
+    *
+    *
+    * */
+
 }
