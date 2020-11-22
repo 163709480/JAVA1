829c7676c1dcb967ef30d78f9dd60f7714c676a7
1637094807 <1637094807@outlook.com>
2020-11-22 20:14:11 +0800

    4

--- src/demo01/Map/Demo01Exception1.java
+++ src/demo01/Map/Demo01Exception1.java
@@ -40,43 +40,81 @@ public class Demo01Exception1 {
 
 
 //        2.多个异常一次捕获，多次处理
-        try{
-            int[] arr={1,2,3};
-           // System.out.println(arr[3]);//
-            List<Integer> list = List.of(1, 2, 3);
-            System.out.println(list.get(3));//ArrayIndexOutOfBoundsException: Index 3 out of bounds for length 3
-
-
-        }catch (ArrayIndexOutOfBoundsException e){//ArrayIndexOutOfBoundsException: Index 3 out of bounds for length 3
-            System.out.println(e);
-
-
-        }catch (IndexOutOfBoundsException e){
-        System.out.println(e);
-
-    }
+//        try{
+//            int[] arr={1,2,3};
+//           // System.out.println(arr[3]);//
+//            List<Integer> list = List.of(1, 2, 3);
+//            System.out.println(list.get(3));//ArrayIndexOutOfBoundsException: Index 3 out of bounds for length 3
+//
+//
+//        }catch (ArrayIndexOutOfBoundsException e){//ArrayIndexOutOfBoundsException: Index 3 out of bounds for length 3
+//            System.out.println(e);
+//
+//
+//        }catch (IndexOutOfBoundsException e){
+//        System.out.println(e);
+//
+//    }
     /*
     *   一个try多个catch注意事项:
     *           catch里面定义的异常变量，如果有子父类关系,那么子类的异常变量必须写在上边,否则就会报错
     *           ArrayIndexOutOfBoundsException e extends IndexOutOfBoundsException
     *
+    *
+    *
+    *
+    *   try中可能会产生的异常对象:
+    *           new ArrayIndexPutOfBoundsException("3");
+    *           new IndexPutOfBoundsException("3");
+    *   try中如果出现了异常,会把异常对象
+    *
     * */
+//        try {
+//            int[] arr = {1, 2, 3};
+//            // System.out.println(arr[3]);//
+//            List<Integer> list = List.of(1, 2, 3);
+//            System.out.println(list.get(3));//ArrayIndexOutOfBoundsException: Index 3 out of bounds for length 3
+//
+//
+//        }catch (IndexOutOfBoundsException e){
+//            System.out.println(e);
+////            }catch (ArrayIndexOutOfBoundsException e){//ArrayIndexOutOfBoundsException: Index 3 out of bounds for length 3
+//                System.out.println(e);
+//
+//
+//            }
+
+        //3.多个异常一次捕获一次处理。
+
+//        try {
+//            int[] arr = {1, 2, 3};
+////             System.out.println(arr[3]);//
+//            List<Integer> list = List.of(1, 2, 3);
+//            System.out.println(list.get(3));//ArrayIndexOutOfBoundsException: Index 3 out of bounds for length 3
+//
+//
+//        }catch (Exception e){
+//            System.out.println(e);
+////
+//
+//            System.out.println("后续代码");
+//
+//        }
+
+        //运行时异常被抛出可以不处理.既不捕获也不声明抛出
+        //默认给虚拟机处理,终止程序,什么时候不抛出运行时异常了，再来继续执行程序
         try {
             int[] arr = {1, 2, 3};
-            // System.out.println(arr[3]);//
+//             System.out.println(arr[3]);//
             List<Integer> list = List.of(1, 2, 3);
             System.out.println(list.get(3));//ArrayIndexOutOfBoundsException: Index 3 out of bounds for length 3
 
 
-        }catch (IndexOutOfBoundsException e){
+        }catch (Exception e){
             System.out.println(e);
-            }catch (ArrayIndexOutOfBoundsException e){//ArrayIndexOutOfBoundsException: Index 3 out of bounds for length 3
-                System.out.println(e);
+            System.out.println("后续代码");
 
-
-            }
-
-
+        }
 
 
 
