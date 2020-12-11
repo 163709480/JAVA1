f52836f6152759a7ed815fb65e87b8cec905a834
1637094807 <1637094807@outlook.com>
2020-12-11 21:10:59 +0800

    4

--- README.md
+++ README.md
@@ -161,5 +161,18 @@ Overide
         如果能获取锁,线程就从WAITING状态变成RUNNABLE状态
         否则,从wait set出来,又进入entry set,线程就从WAITING状态又变成BLOCKED状态
         
+        
+        
+        
+        线程池的概念和原理
+        当程序第一次启动的时候,创建多个线程,保存到一个集合中
+        当我们想要使用线程的时候,就可以从集合中取出来线程使用
+        Thread t=list.remove(0); 返回的是被移除的元素,(线程只能被一个任务使用)
+        Thread t = linked.removeFist();
+        当我们使用完毕线程,需要把线程归还给线程池
+        list.add(t)
+        linked.addLast(t);
+        
+        JDK1.5之后内置了线程池,我们可以直接使用
 
 <HTML>
---
+++ src/Demo01ThreadPool/Demo01ThreadPool.java
@@ -0,0 +1,45 @@
+package Demo01ThreadPool;
+/*
+*
+*   线程池:jdk1.5之后提供的
+*   java.tuil.concurrent.Excetors:线程池的工厂类,用来生成线程池
+*   Executors类中的静态方法:
+*           static ExecutorService newFixedThreadPool(int nThreads)创建一个可重复固定线程数的线程池
+*           参数:
+*               int nThreads:创建线程池中包含的线程数量
+*           返回值ExecutorService接口,返回的是ExecutorService接口的实现类对象,我们可以使用ExecutorService接口接受(面向接口编程)
+*   java.util.concurrent.ExecutorService：线程池接口
+*       用来从线程池中获取线程,调用start方法,执行线程任务
+*           submit(Runnable task)提交一个 Runnable任务用于执行
+*       关闭/销毁线程池的方法
+*           void shutdown()
+*   线程池的使用步骤
+*       1.使用线程池的工厂类Exectors里面提供的静态方法newFixedThreadPool生产一个指定线程数量的线程池
+*       2.创建一个类,实现Runnable接口,重写run方法,设置线程任务
+*       3.调用ExecutorService中的方法submit,传递线程任务(实现类),开启线程,执行run方法
+*       4.调用ExecutorService中的方法shutdown销毁线程池(不建议执行)
+*
+* */
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Demo01ThreadPool {
+    public static void main(String[] args) {
+        // 1.使用线程池的工厂类Exectors里面提供的静态方法newFixedThreadPool生产一个指定线程数量的线程池
+        ExecutorService es= Executors.newFixedThreadPool(2);
+        //       3.调用ExecutorService中的方法submit,传递线程任务(实现类),开启线程,执行run方法
+        es.submit(new RunnableImpi());//pool-1-thread-1创建了一个新的线程执行
+        //线程池会一直开启,使用完了线程,会自动把线程归还给线程池,线程可以继续使用
+        es.submit(new RunnableImpi());//pool-1-thread-1创建了一个新的线程执行
+        es.submit(new RunnableImpi());//pool-1-thread-1创建了一个新的线程执行
+
+        es.shutdown();
+        es.submit(new RunnableImpi());//抛异常,线程池都没有了,就不能获取线程池了
+
+    }
+
+
+
+
+}
