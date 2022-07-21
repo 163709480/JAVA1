5274f1260524be7cc6648d5870c0251d8c06c3a8
qw163 <123345>
2022-07-21 16:38:47 +0800

    5

---
+++ src/DEMO/NEW/NEW.java
@@ -0,0 +1,18 @@
+package DEMO.NEW;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+
+
+public class NEW {
+    // main entrer
+    public static void main(String[] args)
+    //main方法
+    {
+
+        System.out.println("hello");
+
+    }
+    
+}
